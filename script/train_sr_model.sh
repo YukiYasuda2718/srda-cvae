d0ef49e974a8fbebbeab27f5b01724295a5111fc
yuki.yasuda <yuki.yasuda.tokyo@gmail.com>
2023-09-30 21:13:18 +0900

    check if sr model training works.

---
+++ script/train_sr_model.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+HOME_DIR=/home/yuki_yasuda/workspace_hub/srda-cvae
+ROOT_DIR=/data1/yuki_yasuda/workspace_hub/srda-cvae
+
+export CUDA_VISIBLE_DEVICES="0,1"
+WORLD_SIZE=2
+SCALE_FACTOR=2
+
+singularity exec \
+        --nv \
+        --bind $ROOT_DIR:$HOME_DIR \
+        --env PYTHONPATH=$HOME_DIR/pytorch \
+    $ROOT_DIR/pytorch.sif python3 $HOME_DIR/pytorch/script/train_sr_model_ddp.py \
+        --scale_factor $SCALE_FACTOR \
+        --world_size $WORLD_SIZE
