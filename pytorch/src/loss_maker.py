f89981a8e358ad349a0ca09278970e41586d5f67
yuki.yasuda <yuki.yasuda.tokyo@gmail.com>
2024-10-21 00:42:01 +0900

    add lr mse as a loss term

--- pytorch/src/loss_maker.py
+++ pytorch/src/loss_maker.py
@@ -21,15 +21,24 @@ class MaskedL1Loss(nn.Module):
 
 
 class VariationalLowerBoundSnapshot(nn.Module):
-    def __init__(self, std_latent: float, std_reconst: float, beta: float, **kwargs):
+    def __init__(
+        self,
+        std_latent: float,
+        std_reconst: float,
+        beta: float,
+        lr_weight: float,
+        **kwargs,
+    ):
         super().__init__()
         self.std_latent = std_latent
         self.std_reconst = std_reconst
         self.beta = beta
+        self.lr_weight = lr_weight
 
         logger.info(f"std_latent = {self.std_latent}")
         logger.info(f"std_reconst = {self.std_reconst}")
         logger.info(f"beta = {self.beta}")
+        logger.info(f"lr_weight = {self.lr_weight}")
 
     def forward(
         self,
@@ -40,13 +49,17 @@ class VariationalLowerBoundSnapshot(nn.Module):
         mu_prior: torch.Tensor,
         obs: torch.Tensor,
         is_obs: torch.Tensor,
+        lr_analysis: torch.Tensor,
+        lr_input: torch.Tensor,
     ) -> torch.Tensor:
 
         kl_div = 2 * np.log(self.std_latent) - torch.mean(logvar)
         kl_div = kl_div + torch.mean(torch.exp(logvar)) / (self.std_latent**2)
         kl_div = kl_div + torch.mean((mu - mu_prior) ** 2) / (self.std_latent**2)
 
+        lr_los = torch.mean((lr_analysis - lr_input) ** 2)
+
         negative_log_likelihood = torch.mean(is_obs * ((obs - obs_reconst) ** 2))
         negative_log_likelihood = negative_log_likelihood / (self.std_reconst**2)
 
-        return self.beta * kl_div + negative_log_likelihood
+        return self.beta * kl_div + negative_log_likelihood + self.lr_weight * lr_los
